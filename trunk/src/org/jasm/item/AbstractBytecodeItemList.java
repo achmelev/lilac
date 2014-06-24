1eb7c9903a8c6284c14564d6c65791798ec517b0
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-06-24 15:46:09 +0000

    item, itemlist

---
+++ trunk/src/org/jasm/item/AbstractBytecodeItemList.java
@@ -0,0 +1,55 @@
+package org.jasm.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jasm.bytebuffer.IByteBuffer;
+
+public abstract class AbstractBytecodeItemList<T extends IBytecodeItem> implements IBytecodeItem {
+	
+	private List<T> items = new ArrayList<>();
+	private int size = 0;
+	
+	@Override
+	public void read(IByteBuffer source, long offset) {
+		items.clear();
+		int currentOffset = 0;
+		size = source.readUnsignedShort(currentOffset);
+		currentOffset+=2;
+		for (int i=0;i<size; i++) {
+			T item = createEmptyItem();
+			item.read(source, currentOffset);
+			items.add(item);
+			currentOffset+=item.getLength();
+		}
+		
+	}
+	
+	@Override
+	public void write(IByteBuffer target, long offset) {
+		int currentOffset = 0;
+		target.writeUnsignedShort(offset, size);
+		currentOffset+=2;
+		for (IBytecodeItem item: items) {
+			item.write(target, currentOffset);
+			currentOffset+=item.getLength();
+		}
+		
+	}
+	
+	@Override
+	public int getLength() {
+		int result = 0;
+		for (IBytecodeItem item: items) {
+			result+=item.getLength();
+		}
+		return result;
+	}
+	
+	protected abstract T createEmptyItem();
+	
+	
+	
+	
+
+}
---
+++ trunk/src/org/jasm/item/IBytecodeItem.java
@@ -0,0 +1,11 @@
+package org.jasm.item;
+
+import org.jasm.bytebuffer.IByteBuffer;
+
+public interface IBytecodeItem {
+	
+	public void read(IByteBuffer source, long offset);
+	public void write(IByteBuffer target, long offset);
+	public int getLength();
+
+}
