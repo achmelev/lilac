a248a0d4356c097cba1a31b5a6a447cf559e4e55
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-16 09:54:16 +0000

    type descriptor

---
+++ src/org/jasm/item/descriptor/IllegalDescriptorException.java
@@ -0,0 +1,12 @@
+package org.jasm.item.descriptor;
+
+public class IllegalDescriptorException extends Exception {
+
+	public IllegalDescriptorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/org/jasm/item/descriptor/TypeDescriptor.java
@@ -0,0 +1,109 @@
+package org.jasm.item.descriptor;
+
+public class TypeDescriptor {
+	
+	private boolean isByte;
+	private boolean isCharacter;
+	private boolean isDouble;
+	private boolean isFloat;
+	private boolean isInteger;
+	private boolean isLong;
+	private boolean isObject;
+	private boolean isArray;
+	private boolean isShort;
+	private boolean isBoolean;
+	
+	private TypeDescriptor componentType;
+	
+	public TypeDescriptor(String descriptor) throws IllegalDescriptorException {
+		isByte = false;
+		isCharacter = false;
+		isDouble = false;
+		isFloat = false;
+		isInteger = false;
+		isLong = false;
+		isObject = false;
+		isArray = false;
+		isShort = false;
+		isBoolean = false;
+		
+		if (descriptor.equals("B")) {
+			isByte = true;
+		} else if (descriptor.equals("C")) {
+			isCharacter = true;
+		} else if (descriptor.equals("D")) {
+			isDouble = true;
+		} else if (descriptor.equals("F")) {
+			isFloat = true;
+		} else if (descriptor.equals("I")) {
+			isInteger = true;
+		} else if (descriptor.equals("J")) {
+			isLong = true;
+		} else if (descriptor.startsWith("L") & descriptor.endsWith(";")) {
+			isObject = true;
+		} else if (descriptor.startsWith("[")) {
+			isArray = true;
+			if (descriptor.length()<2) {
+				throw new IllegalDescriptorException("illegal type descriptor: "+descriptor);
+			}
+			componentType = new TypeDescriptor(descriptor.substring(1, descriptor.length()));
+		} else if (descriptor.equals("S")) {
+			isShort = true;
+		} else if (descriptor.equals("Z")) {
+			isBoolean = true;
+		} else {
+			throw new IllegalDescriptorException(("illegal type descriptor: "+descriptor));
+		}
+	}
+
+	public boolean isByte() {
+		return isByte;
+	}
+
+	public boolean isCharacter() {
+		return isCharacter;
+	}
+
+	public boolean isDouble() {
+		return isDouble;
+	}
+
+	public boolean isFloat() {
+		return isFloat;
+	}
+	
+	public boolean isInteger() {
+		return isInteger;
+	}
+
+	public boolean isLong() {
+		return isLong;
+	}
+
+	public boolean isObject() {
+		return isObject;
+	}
+
+	public boolean isArray() {
+		return isArray;
+	}
+
+	public boolean isShort() {
+		return isShort;
+	}
+
+	public boolean isBoolean() {
+		return isBoolean;
+	}
+
+	public TypeDescriptor getComponentType() {
+		if (!isArray()) {
+			throw new IllegalStateException("isn't an array!");
+		}
+		return componentType;
+	}
+	
+	
+	
+
+}
