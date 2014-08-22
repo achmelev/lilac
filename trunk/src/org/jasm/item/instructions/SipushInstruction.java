d97815426bb056e995e383905d61099c050d9d2c
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-22 21:13:42 +0000

    sipush

---
+++ trunk/src/org/jasm/item/instructions/SipushInstruction.java
@@ -0,0 +1,57 @@
+package org.jasm.item.instructions;
+
+import java.util.List;
+
+import org.jasm.bytebuffer.IByteBuffer;
+import org.jasm.bytebuffer.print.IPrintable;
+
+public class SipushInstruction extends AbstractInstruction {
+	
+	private short value = -1;
+	
+	public SipushInstruction(short value) {
+		super(OpCodes.sipush);
+		this.value = value;
+	}
+
+	@Override
+	public void read(IByteBuffer source, long offset) {
+		this.value = source.readShort(offset);
+	}
+
+	@Override
+	public void write(IByteBuffer target, long offset) {
+		target.writeShort(offset, value);
+	}
+
+	@Override
+	public int getLength() {
+		return 3;
+	}
+
+	@Override
+	public boolean isStructure() {
+		return false;
+	}
+
+	@Override
+	public List<IPrintable> getStructureParts() {
+		return null;
+	}
+
+	@Override
+	public String getPrintArgs() {
+		return ""+value;
+	}
+
+	@Override
+	public String getPrintComment() {
+		return null;
+	}
+
+	@Override
+	protected void doResolve() {
+
+	}
+
+}
