2ef5d805e937f5c6dfff1e9d5f9a1ce73759ba8e
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-26 15:28:01 +0000

    Listener, version parsing

--- src/org/jasm/parser/AssemblerParser.java
+++ src/org/jasm/parser/AssemblerParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BaseErrorListener;
@@ -11,15 +12,23 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.jasm.item.IBytecodeItem;
 import org.jasm.item.clazz.Clazz;
+import org.jasm.parser.JavaAssemblerParser.ClazzContext;
+import org.jasm.parser.JavaAssemblerParser.VersionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AssemblerParser {
+public class AssemblerParser  extends JavaAssemblerBaseListener {
 	
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	List<ErrorMessage> errorMessages = new ArrayList<>();
+	private List<ErrorMessage> errorMessages = new ArrayList<>();
+	
+	private Clazz result = null;
+	
+	Stack<IBytecodeItem> stack = new Stack<>();
 
 	public List<ErrorMessage> getErrorMessages() {
 		return errorMessages;
@@ -27,6 +36,10 @@ public class AssemblerParser {
 	
 	
 	public Clazz parse(InputStream inp) {
+		
+		result = null;
+		errorMessages.clear();
+		stack.clear();
 		
 		ANTLRInputStream input = null;
 		try {
@@ -40,10 +53,38 @@ public class AssemblerParser {
 		JavaAssemblerParser parser = new JavaAssemblerParser(tokens);
 		parser.removeErrorListeners();
 		parser.addErrorListener(new SyntaxErrorListener(this));
+		
+		//Parse
 		ParseTree tree = parser.clazz();
 		
 		if (log.isDebugEnabled()) {
 			log.debug("tree: "+tree.toStringTree());
+		}
+		debugErrors();
+		
+		
+		
+		if (errorMessages.size() == 0) {
+			//Walk tree an create class
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.walk(this, tree);
+			debugErrors();
+			if (errorMessages.size() == 0) {
+				//resolve and validate symbolic references
+				//TODO
+				
+			} else {
+				result = null;
+			}
+		}
+		
+		
+		
+		return result;
+	}
+	
+	private void debugErrors() {
+		if (log.isDebugEnabled()) {
 			if (getErrorMessages().size() > 0) {
 				StringBuffer buf = new StringBuffer();
 				for (ErrorMessage erm: getErrorMessages()) {
@@ -53,9 +94,36 @@ public class AssemblerParser {
 				log.debug("\nsyntax error messages:\n"+buf.toString());
 			}
 		}
-		
-		return null;
 	}
+
+
+	@Override
+	public void enterClazz(ClazzContext ctx) {
+		stack.push(new Clazz());
+	}
+
+
+	@Override
+	public void exitClazz(ClazzContext ctx) {
+		result = (Clazz)stack.pop();
+	}
+
+
+	@Override
+	public void enterVersion(VersionContext ctx) {
+		Clazz clazz = (Clazz)stack.peek();
+		String version = ctx.VersionLiteral().getText();
+		int lineNumber = ctx.VersionLiteral().getSymbol().getLine();
+		int charPosition = ctx.VersionLiteral().getSymbol().getCharPositionInLine();
+		try {
+			clazz.setVersion(version);
+		} catch (IllegalArgumentException e) {
+			errorMessages.add(new ErrorMessage(lineNumber, charPosition, e.getMessage()));
+		}
+	}
+	
+	
+	
 	
 	
 
