38c65c32b9355cf75d8e6369d06cdb92348a51c5
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-26 09:02:30 +0000

    Error listener

--- trunk/src/org/jasm/parser/AssemblerParser.java
+++ trunk/src/org/jasm/parser/AssemblerParser.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.jasm.item.clazz.Clazz;
 import org.slf4j.Logger;
@@ -16,9 +19,9 @@ public class AssemblerParser {
 	
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	List<String> errorMessages = new ArrayList<>();
+	List<ErrorMessage> errorMessages = new ArrayList<>();
 
-	public List<String> getErrorMessages() {
+	public List<ErrorMessage> getErrorMessages() {
 		return errorMessages;
 	}
 	
@@ -35,10 +38,20 @@ public class AssemblerParser {
 		JavaAssemblerLexer lexer = new JavaAssemblerLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		JavaAssemblerParser parser = new JavaAssemblerParser(tokens);
+		parser.removeErrorListeners();
+		parser.addErrorListener(new SyntaxErrorListener(this));
 		ParseTree tree = parser.clazz();
 		
 		if (log.isDebugEnabled()) {
 			log.debug("tree: "+tree.toStringTree());
+			if (getErrorMessages().size() > 0) {
+				StringBuffer buf = new StringBuffer();
+				for (ErrorMessage erm: getErrorMessages()) {
+					buf.append(erm.toString());
+					buf.append("\n");
+				}
+				log.debug("\nsyntax error messages:\n"+buf.toString());
+			}
 		}
 		
 		return null;
@@ -46,4 +59,21 @@ public class AssemblerParser {
 	
 	
 
+}
+
+class SyntaxErrorListener extends BaseErrorListener {
+	
+	private AssemblerParser parent;
+	
+	SyntaxErrorListener(AssemblerParser parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public void syntaxError(Recognizer<?, ?> recognizer,
+			Object offendingSymbol, int line, int charPositionInLine,
+			String msg, RecognitionException e) {
+		parent.getErrorMessages().add(new ErrorMessage(line, charPositionInLine, msg));
+	}
+	
 }
