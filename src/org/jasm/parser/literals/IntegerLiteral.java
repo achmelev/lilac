3a3df5ffa5c510f217e5d39c6813c396602c7c0a
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-31 20:12:18 +0000

    integer literal with unit test

---
+++ src/org/jasm/parser/literals/IntegerLiteral.java
@@ -0,0 +1,49 @@
+package org.jasm.parser.literals;
+
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+public class IntegerLiteral extends AbstractLiteral {
+	
+	public IntegerLiteral(int line, int charPosition, String content) {
+		super(line, charPosition, content);
+	}
+	
+	public boolean isValid() {
+		try {
+			getValue();
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	
+	public int getValue() throws NumberFormatException {
+		String content = getContent();
+		char sign='+';
+		int result = -1;
+		if (content.charAt(0)=='-' || content.charAt(0)=='+') {
+			sign=content.charAt(0);
+			content = content.substring(1, content.length());
+		}
+		
+		if (content.startsWith("0x") || content.startsWith("0X")) { //hexadecimal
+			result = Integer.parseInt(sign+content.substring(2, content.length()),16);
+		} else if (content.startsWith("0b") || content.startsWith("0B")) { //hexadecimal
+			result = Integer.parseInt(sign+content.substring(2, content.length()),2);
+		} else if (content.startsWith("0")) {//oktal
+			result = Integer.parseInt(sign+content,8);
+		} else {
+			result = Integer.parseInt(sign+content,10);
+		}
+		
+		if ((sign == '+' && result<0) ||  (sign == '-' && result>0)) {
+			throw new NumberFormatException("input string "+getContent());
+		}
+		
+		return result;
+	}
+
+}
