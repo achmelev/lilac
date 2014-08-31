4229368f8e1b6028eeea2a9f0d54bf0fba899a0a
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-08-31 16:03:45 +0000

    unit test - escaping in string literals

--- trunk/src-test/org/jasm/test/parser/Class1ParserTest.java
+++ trunk/src-test/org/jasm/test/parser/Class1ParserTest.java
@@ -46,6 +46,7 @@ public class Class1ParserTest extends AbstractParserTestCase {
 		Assert.assertEquals(1,clazz.getConstantPool().getNameAndTypeInfos("STRING_CONSTANT", "Ljava/lang/String;").size());
 		Assert.assertEquals(1,clazz.getConstantPool().getMethodRefs("java/lang/Object","<init>", "()V").size());
 		Assert.assertEquals(1,clazz.getConstantPool().getStringEntries("HELLO \n \\WORLD\r\t").size());
+		
 	}
 
 }
