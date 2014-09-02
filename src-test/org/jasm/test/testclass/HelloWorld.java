0ae9fc1d3c502ff2c93833c2c623117c3d522c82
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-09-02 21:08:49 +0000

    another disassembling comments improvements

--- src-test/org/jasm/test/testclass/HelloWorld.java
+++ src-test/org/jasm/test/testclass/HelloWorld.java
@@ -3,7 +3,7 @@ package org.jasm.test.testclass;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello \n World!");
 	}
 
 }
