7062ceb1bd151a6fe729ed4c947bff07a458480e
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-09-27 18:59:13 +0000

    streamlined the disassembling of the parameter annotations

--- trunk/src/org/jasm/item/attribute/ParameterAnnotations.java
+++ trunk/src/org/jasm/item/attribute/ParameterAnnotations.java
@@ -7,12 +7,7 @@ public class ParameterAnnotations extends AbstractBytecodeItemList<Annotation> {
 
 	@Override
 	public String getPrintName() {
-		StringBuffer buf = new StringBuffer();
-		if (this.getParent() instanceof RuntimeInvisibleParameterAnnotationsAttributeContent) {
-			buf.append("invisible ");
-		} 
-		buf.append("parameter annotations");
-		return  buf.toString();
+		return null;
 	}
 	
 	@Override
