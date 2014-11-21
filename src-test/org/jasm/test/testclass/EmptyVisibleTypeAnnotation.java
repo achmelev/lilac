3a9df19e70d67016d31d7e1a9867a2dce9f738c7
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-11-21 22:33:21 +0000

    type annotations: empty target

--- src-test/org/jasm/test/testclass/EmptyVisibleTypeAnnotation.java
+++ src-test/org/jasm/test/testclass/EmptyVisibleTypeAnnotation.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD,ElementType.LOCAL_VARIABLE,ElementType.METHOD,ElementType.PARAMETER,ElementType.TYPE_PARAMETER,ElementType.TYPE_USE,ElementType.TYPE,ElementType.ANNOTATION_TYPE})
-public @interface EmptyTypedInvisibleAnnotation {
+public @interface EmptyVisibleTypeAnnotation {
 
 }
---
+++ src-test/org/jasm/test/testclass/TypeAnnotationClass.java
@@ -0,0 +1,25 @@
+package org.jasm.test.testclass;
+
+public class TypeAnnotationClass implements  Runnable {
+	
+	private @EmptyInvisibleTypeAnnotation @EmptyVisibleTypeAnnotation String text;
+
+	@Override
+	public void run() {
+		
+		
+	}
+
+	@Override
+	public @EmptyVisibleTypeAnnotation @EmptyInvisibleTypeAnnotation String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	public void send(@EmptyVisibleTypeAnnotation @EmptyInvisibleTypeAnnotation TypeAnnotationClass this) {
+		
+	}
+	
+	
+
+}
