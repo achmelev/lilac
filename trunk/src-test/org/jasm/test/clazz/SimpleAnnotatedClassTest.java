f573ce29d791c88ebd1669940eceac947e66b332
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-11-22 13:35:21 +0000

    some unit tests

---
+++ trunk/src-test/org/jasm/test/clazz/SimpleAnnotatedClassTest.java
@@ -0,0 +1,69 @@
+package org.jasm.test.clazz;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.jasm.bytebuffer.ByteArrayByteBuffer;
+import org.jasm.bytebuffer.print.PrettyPrinter;
+import org.jasm.item.attribute.Attributes;
+import org.jasm.item.clazz.Clazz;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleAnnotatedClassTest {
+	
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	public void test() {
+		ClassLoader cl = this.getClass().getClassLoader();
+		InputStream stream = cl.getResourceAsStream("org/jasm/test/testclass/SimpleAnnotatedClass.class");
+		ByteArrayOutputStream bo = new ByteArrayOutputStream();
+		byte [] buf = new byte[1024];
+		try {
+			int read = stream.read(buf);
+			while (read>=0) {
+				if (read>0) {
+					bo.write(buf, 0, read);
+				}
+				read = stream.read(buf);
+			}
+			bo.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		byte [] data = bo.toByteArray();
+		ByteArrayByteBuffer bbuf = new ByteArrayByteBuffer(data);
+		
+		Clazz clazz = new Clazz();
+		clazz.read(bbuf, 0L);
+		clazz.resolve();
+		clazz.updateMetadata();
+		
+		StringWriter sw = new StringWriter();
+		PrintWriter writer = new PrintWriter(sw);
+		PrettyPrinter printer = new PrettyPrinter(writer);
+		printer.printItem(clazz);
+		writer.close();
+		log.debug("code: \n"+sw.toString());
+		
+		
+		
+		
+		byte [] data2 = new byte[clazz.getLength()];
+		ByteArrayByteBuffer bbuf2 = new ByteArrayByteBuffer(data2);
+		clazz.write(bbuf2, 0);
+		assertArrayEquals(data2, data);
+		
+	}
+	
+	
+
+}
