5417c142f2fdfbcdfa49311ed7746ca77b105350
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-12-04 21:20:33 +0000

    refactoring read/write tests

---
+++ trunk/src-test/org/jasm/test/readwrite/GenericClassTest1.java
@@ -0,0 +1,45 @@
+package org.jasm.test.readwrite;
+
+import org.jasm.item.attribute.Attributes;
+import org.jasm.item.attribute.IAttributeContent;
+import org.jasm.item.attribute.SignatureAttributeContent;
+import org.jasm.item.attribute.SourceFileAttributeContent;
+import org.jasm.item.clazz.Clazz;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericClassTest1 extends AbstractReadWriteTest {
+	
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	public void test() {
+		doTest();
+		
+	}
+	
+	private <T extends IAttributeContent> T getAttributeContent(Attributes attrs, Class<T> clazz) {
+		for (int i=0;i<attrs.getSize(); i++) {
+			if (attrs.get(i).getContent().getClass().equals(clazz)) {
+				return (T) attrs.get(i).getContent();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	protected String getClassResourceName() {
+		return "org/jasm/test/testclass/GenericClass.class";
+	}
+
+	@Override
+	protected void doChecks(Clazz clazz) {
+		Assert.assertEquals("<T:Ljava/lang/Object;>Ljava/lang/Object;", getAttributeContent(clazz.getAttributes(), SignatureAttributeContent.class).getValue());
+		Assert.assertEquals("GenericClass.java", getAttributeContent(clazz.getAttributes(), SourceFileAttributeContent.class).getValue());
+		Assert.assertEquals("(TT;)TT;", getAttributeContent(clazz.getMethods().get(1).getAttributes(), SignatureAttributeContent.class).getValue());
+		
+	}
+
+}
---
+++ trunk/src-test/org/jasm/test/readwrite/HelloWorldTestTest.java
@@ -0,0 +1,28 @@
+package org.jasm.test.readwrite;
+
+import org.jasm.item.clazz.Clazz;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorldTestTest extends AbstractReadWriteTest {
+	
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	public void test() {
+		doTest();
+		
+	}
+
+	@Override
+	protected String getClassResourceName() {
+		return "org/jasm/test/testclass/HelloWorld.class";
+	}
+
+	@Override
+	protected void doChecks(Clazz clazz) {
+		
+	}
+
+}
