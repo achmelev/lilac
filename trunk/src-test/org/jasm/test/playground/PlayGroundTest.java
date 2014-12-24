a8ba341646c7611a34f03aee3b61946a5f4e25b5
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2014-12-24 13:44:58 +0000

    AssemblerClassLoader, playground

---
+++ trunk/src-test/org/jasm/test/playground/PlayGroundTest.java
@@ -0,0 +1,25 @@
+package org.jasm.test.playground;
+
+import org.jasm.loader.AssemblerClassLoader;
+import org.junit.Test;
+
+public class PlayGroundTest {
+	
+	@Test
+	public void testCalculator() {
+		ClassLoader cl = createClassLoader();
+		try {
+			Class clazz = cl.loadClass(SimpleCalculator.class.getName());
+			ICalculator calc = (ICalculator)clazz.newInstance();
+			calc.calculate(3, 4);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	private ClassLoader createClassLoader() {
+		return new AssemblerClassLoader(Thread.currentThread().getContextClassLoader());
+	}
+
+}
