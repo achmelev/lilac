9044933ad13b54ef200c9664284f4a748a794667
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2015-01-01 10:09:53 +0000

    refactoring jar-Unittests, Runtime6 - unit tests

---
+++ trunk/src-test/org/jasm/test/jar/AbstractReadWriteJarTestCase.java
@@ -0,0 +1,56 @@
+package org.jasm.test.jar;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+
+import org.jasm.bytebuffer.ByteArrayByteBuffer;
+import org.jasm.bytebuffer.print.PrettyPrinter;
+import org.jasm.item.clazz.Clazz;
+import org.jasm.parser.AssemblerParser;
+import org.jasm.resolver.ClassInfoResolver;
+import org.jasm.resolver.ClassLoaderClasspathEntry;
+import org.jasm.resolver.JarFileClassPathEntry;
+import org.jasm.type.verifier.VerifierParams;
+import org.junit.Assert;
+
+public abstract class AbstractReadWriteJarTestCase extends
+		AbstractJarTestCase {
+	
+	@Override
+	protected void testClass(byte[] data, File jarFile) {
+		
+		byte[] data2 = readWrite(data);
+		assertArrayEquals(data, data2);
+	}
+	
+	protected byte[] readWrite(byte [] data) {
+		ByteArrayByteBuffer bbuf = new ByteArrayByteBuffer(data);
+		
+		Clazz clazz = new Clazz();
+		clazz.read(bbuf, 0L);
+		clazz.resolve();
+		clazz.updateMetadata();
+		
+		byte [] data2 = new byte[clazz.getLength()];
+		ByteArrayByteBuffer bbuf2 = new ByteArrayByteBuffer(data2);
+		clazz.write(bbuf2, 0);
+		
+		return data2;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
