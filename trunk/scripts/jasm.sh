08e5a3454f20bf74d7f0b1c7d73e10e7b91ad106
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2015-09-10 11:36:50 +0000

    Fix - spaces allowed in installation dir and JRE home

--- trunk/scripts/jasm.sh
+++ trunk/scripts/jasm.sh
@@ -6,7 +6,7 @@ else
 	JASM_JAVA_CMD=$JASM_JAVA_HOME/bin/java
 	if [ -f $JASM_JAVA_CMD ]; then 
 		JASM_INSTALL_DIR=`dirname "$0"`
-		$JASM_JAVA_CMD -classpath $JASM_INSTALL_DIR/jasm.jar org.jasm.tools.Assembler $*
+		"$JASM_JAVA_CMD" -classpath "$JASM_INSTALL_DIR/jasm.jar" org.jasm.tools.Assembler $*
 	else 
 		echo "Couldn't find $JASM_JAVA_CMD! Please adjust JASM_JAVA_HOME to the correct value."
 	fi
