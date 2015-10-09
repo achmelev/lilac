51d490541b0dc48603850d80a372094334cfd54a
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2015-10-09 05:47:30 +0000

    Unit test for annotation target path fix Some examples

---
+++ trunk/src-test/org/jasm/test/testclass/TargetPathAnnotation.java
@@ -0,0 +1,9 @@
+package org.jasm.test.testclass;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+public @interface TargetPathAnnotation {
+
+}
