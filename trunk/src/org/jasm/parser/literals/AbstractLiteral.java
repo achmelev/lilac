076019eb914845bdde5a464979aaab53667a134b
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2015-11-21 08:06:26 +0000

    Source locations in literals

--- trunk/src/org/jasm/parser/literals/AbstractLiteral.java
+++ trunk/src/org/jasm/parser/literals/AbstractLiteral.java
@@ -1,14 +1,19 @@
 package org.jasm.parser.literals;
 
+import org.jasm.parser.SourceLocation;
+
 public abstract class AbstractLiteral {
 	private int line = -1;
 	private int charPosition = -1;
 	private String content;
 	
+	private SourceLocation sourceLocation;
+	
 	public AbstractLiteral(int line, int charPosition, String content) {
 		this.line = line;
 		this.charPosition = charPosition;
 		this.content = content;
+		this.sourceLocation = new SourceLocation(line, charPosition);
 	}
 
 	public int getLine() {
@@ -30,6 +35,10 @@ public abstract class AbstractLiteral {
 	@Override
 	public String toString() {
 		return getContent();
+	}
+
+	public SourceLocation getSourceLocation() {
+		return sourceLocation;
 	}
 	
 	
