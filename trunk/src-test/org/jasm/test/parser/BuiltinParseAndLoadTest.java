0a0bfcfb5ef0bea58743f5965ab563ef117c2da6
alexei_chmelev <alexei_chmelev@e5ea2dcd-1b36-49cb-b898-916444ed6db1>
2015-12-26 09:42:47 +0000

    cast - unit tests primitive to primitive

--- trunk/src-test/org/jasm/test/parser/BuiltinParseAndLoadTest.java
+++ trunk/src-test/org/jasm/test/parser/BuiltinParseAndLoadTest.java
@@ -37,7 +37,79 @@ public class BuiltinParseAndLoadTest extends AbstractParseAndLoadTestCase {
 			Assert.assertEquals(new Integer(1), instance.boxZ2I(true));
 			Assert.assertEquals(true, instance.unbox(Boolean.TRUE));
 			Assert.assertEquals(1, instance.unboxZ2I(Boolean.TRUE));
+			
+			//Promitive casts
+			Assert.assertEquals(1, instance.pconvert1(true));
+			Assert.assertEquals(1, instance.pconvert2((char)1));
+			Assert.assertEquals(1, instance.pconvert3(1.0));
+			Assert.assertEquals(1, instance.pconvert4(1.0f));
+			Assert.assertEquals(1, instance.pconvert5(1));
+			Assert.assertEquals(1, instance.pconvert6(1L));
+			Assert.assertEquals(1, instance.pconvert7((short)1));
+			
+			Assert.assertEquals(false, instance.pconvert8((byte)0));
+			Assert.assertEquals(true, instance.pconvert9((' ')));
+			Assert.assertEquals(true, instance.pconvert10(1.0));
+			Assert.assertEquals(true, instance.pconvert11(1.0f));
+			Assert.assertEquals(false, instance.pconvert12(0));
+			Assert.assertEquals(false, instance.pconvert13(0L));
+			Assert.assertEquals(false, instance.pconvert14((short)0));
+			
+			Assert.assertEquals(' ', instance.pconvert15((byte)32));
+			Assert.assertEquals((char)1, instance.pconvert16(true));
+			Assert.assertEquals(' ', instance.pconvert17(32.0));
+			Assert.assertEquals(' ', instance.pconvert18(32.0f));
+			Assert.assertEquals(' ', instance.pconvert19(32));
+			Assert.assertEquals(' ', instance.pconvert20(32L));
+			Assert.assertEquals(' ', instance.pconvert21((short)32));
+			
+			Assert.assertEquals(5.0, instance.pconvert22((byte)5));
+			Assert.assertEquals(1.0, instance.pconvert23(true));
+			Assert.assertEquals(32.0, instance.pconvert24(' '));
+			Assert.assertEquals(10.0, instance.pconvert25(10.0f));
+			Assert.assertEquals(10.0, instance.pconvert26(10));
+			Assert.assertEquals(10.0, instance.pconvert27(10L));
+			Assert.assertEquals(10.0, instance.pconvert28((short)10));
+			
+			Assert.assertEquals(5.0f, instance.pconvert29((byte)5));
+			Assert.assertEquals(1.0f, instance.pconvert30(true));
+			Assert.assertEquals(32.0f, instance.pconvert31(' '));
+			Assert.assertEquals(10.0f, instance.pconvert32(10.0));
+			Assert.assertEquals(10.0f, instance.pconvert33(10));
+			Assert.assertEquals(10.0f, instance.pconvert34(10L));
+			Assert.assertEquals(10.0f, instance.pconvert35((short)10));
+			
+			Assert.assertEquals(5, instance.pconvert36((byte)5));
+			Assert.assertEquals(1, instance.pconvert37(true));
+			Assert.assertEquals(32, instance.pconvert38(' '));
+			Assert.assertEquals(10, instance.pconvert39(10.1));
+			Assert.assertEquals(10, instance.pconvert40(10.3f));
+			Assert.assertEquals(10, instance.pconvert41(10L));
+			Assert.assertEquals(10, instance.pconvert42((short)10));
+			
+			Assert.assertEquals(5L, instance.pconvert43((byte)5));
+			Assert.assertEquals(1L, instance.pconvert44(true));
+			Assert.assertEquals(32L, instance.pconvert45(' '));
+			Assert.assertEquals(10L, instance.pconvert46(10.1));
+			Assert.assertEquals(10L, instance.pconvert47(10.3f));
+			Assert.assertEquals(10L, instance.pconvert48(10));
+			Assert.assertEquals(10L, instance.pconvert49((short)10));
+			
+			Assert.assertEquals((short)5, instance.pconvert50((byte)5));
+			Assert.assertEquals((short)1, instance.pconvert51(true));
+			Assert.assertEquals((short)32, instance.pconvert52(' '));
+			Assert.assertEquals((short)10, instance.pconvert53(10.1));
+			Assert.assertEquals((short)10, instance.pconvert54(10.3f));
+			Assert.assertEquals((short)10, instance.pconvert55(10));
+			Assert.assertEquals((short)10, instance.pconvert56(10L));
+			
+			
+			
+			
+			
+			
 		} catch (Exception e) {
+			
 			throw new RuntimeException(e);
 		} 
 		
--- trunk/src-test/org/jasm/test/testclass/IBuiltinMacros.java
+++ trunk/src-test/org/jasm/test/testclass/IBuiltinMacros.java
@@ -10,4 +10,121 @@ public interface IBuiltinMacros {
 	public boolean unbox(Boolean a);
 	
 	public int unboxZ2I(Boolean a);
+	
+	/* PRIMITIVE CASTS */ 
+	
+	//byte, boolean, char,double, float, int, long, short
+	
+	public byte pconvert1(boolean p);
+	
+	public byte pconvert2(char p);
+	
+	public byte pconvert3(double p); 
+	
+	public byte pconvert4(float p); 
+	
+	public byte pconvert5(int p);
+
+	
+	public byte pconvert6(long p);
+	
+	public byte pconvert7(short p); 
+	
+	public boolean pconvert8(byte p); 
+	
+	public boolean pconvert9(char p); 
+	
+	public boolean pconvert10(double p);
+	
+	public boolean pconvert11(float p); 
+	
+	public boolean pconvert12(int p);
+	
+	public boolean pconvert13(long p);
+	
+	public boolean pconvert14(short p); 
+	
+	public char pconvert15(byte p); 
+	
+	public char pconvert16(boolean p); 
+	
+	public char pconvert17(double p);
+	
+	public char pconvert18(float p); 
+	
+	public char pconvert19(int p);
+	
+	public char pconvert20(long p); 
+	
+	public char pconvert21(short p); 
+	
+	public double pconvert22(byte p); 
+	
+	public double pconvert23(boolean p);
+	
+	public double pconvert24(char p); 
+	
+	public double pconvert25(float p);
+	
+	public double pconvert26(int p); 
+	
+	public double pconvert27(long p);
+	
+	public double pconvert28(short p);
+	
+	public float pconvert29(byte p);
+	
+	public float pconvert30(boolean p);
+	
+	public float pconvert31(char p);
+	
+	public float pconvert32(double p); 
+	
+	public float pconvert33(int p);
+	
+	public float pconvert34(long p);
+	
+	public float pconvert35(short p);
+	
+	public int pconvert36(byte p); 
+	
+	public int pconvert37(boolean p);
+	
+	public int pconvert38(char p);
+	
+	public int pconvert39(double p); 
+	
+	public int pconvert40(float p); 
+	
+	public int pconvert41(long p); 
+	
+	public int pconvert42(short p); 
+	
+	public long pconvert43(byte p);
+	
+	public long pconvert44(boolean p); 
+	
+	public long pconvert45(char p); 
+	
+	public long pconvert46(double p); 
+	
+	public long pconvert47(float p);
+	
+	public long pconvert48(int p);
+	
+	public long pconvert49(short p);
+	
+	public short pconvert50(byte p); 
+	
+	public short pconvert51(boolean p);
+	
+	public short pconvert52(char p); 
+	
+	public short pconvert53(double p); 
+	
+	public short pconvert54(float p);
+	
+	public short pconvert55(int p);
+	
+	public short pconvert56(long p); 
 }
